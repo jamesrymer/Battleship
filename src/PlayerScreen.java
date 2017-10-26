3807bdcfe30f1c74030c6999b9be629f18834e22
jamesrymer <rymerjamess@gmail.com>
2017-10-26 10:57:26 -0700

    Update PlayerScreen.java

--- src/PlayerScreen.java
+++ src/PlayerScreen.java
@@ -10,22 +10,7 @@ public class PlayerScreen extends JFrame {
     public PlayerScreen(String name, boolean show) {
         super(name);
         this.setLayout(new BorderLayout());
-        Container pane = new Container();
-        pane.setLayout(new BoxLayout(pane,BoxLayout.Y_AXIS));
-        JButton btnNumberOfShips = new JButton("Number of Own Ships");
-        JButton btnShipsLeft= new JButton("Own ships sunk");
-        JButton btnEnemySunk = new JButton("Number of Enemy Ships Sunk");
-        pane.add(btnNumberOfShips);
-        pane.add(btnShipsLeft);
-        pane.add(btnEnemySunk);
-        
-        
-        btnNumberOfShips.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent f){
-                JLabel displayNumberOfShips = new JLabel("You have [] ships");
-                pane.add(displayNumberOfShips);
-            }
-        });
+       
             
         
         
@@ -48,7 +33,7 @@ public class PlayerScreen extends JFrame {
         });
 
         
-        this.add(pane,BorderLayout.SOUTH);
+       
         
         this.add(new SelfGrid(name), BorderLayout.EAST);
         this.add(new AttackGrid(name), BorderLayout.WEST);

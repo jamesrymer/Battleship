8c63d5af5ea7078c69772e2f87e9058d767d5688
James Rymer <jamesrymer@Jamess-MacBook-Pro-3.local>
2017-10-26 10:49:20 -0700

    Added buttons to player screen

--- src/PlayerScreen.java
+++ src/PlayerScreen.java
@@ -10,10 +10,29 @@ public class PlayerScreen extends JFrame {
         super(name);
         this.setLayout(new BorderLayout());
         
+        Container pane = new Container();
+        pane.setLayout(new BoxLayout(pane,BoxLayout.Y_AXIS));
+        JButton btnNumberOfShips = new JButton("Number of Own Ships");
+        JButton btnShipsLeft= new JButton("Own ships sunk");
+        JButton btnEnemySunk = new JButton("Number of Enemy Ships Sunk");
+        pane.add(btnNumberOfShips);
+        pane.add(btnShipsLeft);
+        pane.add(btnEnemySunk);
+        
+        
+        btnNumberOfShips.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent f){
+                JLabel displayNumberOfShips = new JLabel("You have [] ships");
+                pane.add(displayNumberOfShips);
+            }
+        });
         
         this.add(new SelfGrid(name), BorderLayout.EAST);
         this.add(new AttackGrid(name), BorderLayout.WEST);
         this.add(new JLabel(name), BorderLayout.NORTH);
+        
+        this.add(pane,BorderLayout.SOUTH);
+        
         JButton next = new JButton("next");
         next.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
